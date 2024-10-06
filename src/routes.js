3812f5ddc04d3c96a1ff91e1f9b0dadfd78eeabf
Aidah Novallia Putri <aidahptr15@gmail.com>
2024-10-06 19:07:40 +0700

    chore: update packages

--- src/routes.js
+++ src/routes.js
@@ -1,45 +1,49 @@
-import { useRoutes } from "react-router-dom";
+import { useRoutes } from 'react-router-dom'
 
-import { commonRoutes, commonMobileRoutes } from "commons/routes.js";
-import userRoutes from "user/routes";
-import roleRoutes from "role/routes";
-import staticPageRoutes from "staticPage/routes";
-import homeRoutes from "home/routes";
-import mataKuliahRoutes from "mataKuliah/routes";
-import programStudiRoutes from "programStudi/routes";
-import kurikulumRoutes from "kurikulum/routes";
-import cPLRoutes from "cPL/routes";
-import laporanCPLRoutes from "laporanCPL/routes";
-import cPMKRoutes from "cPMK/routes";
-import subCPMKRoutes from "subCPMK/routes";
-import laporanCPMKRoutes from "laporanCPMK/routes";
-import semesterRoutes from "semester/routes";
-import kelasRoutes from "kelas/routes";
+import { commonRoutes, commonMobileRoutes } from '@/commons/routes'
+import userRoutes from '@/user/routes'
+import roleRoutes from '@/role/routes'
+import staticPageRoutes from '@/staticPage/routes'
+import homeRoutes from '@/home/routes' 
+import kurikulumRoutes from '@/kurikulum/routes' 
+import programStudiRoutes from '@/programStudi/routes' 
+import kelasRoutes from '@/kelas/routes' 
+import semesterRoutes from '@/semester/routes' 
+import riwayatAkademisRoutes from '@/riwayatAkademis/routes' 
+import cPMKRoutes from '@/cPMK/routes' 
+import subCPMKRoutes from '@/subCPMK/routes' 
+import laporanCPMKRoutes from '@/laporanCPMK/routes' 
+import cPLRoutes from '@/cPL/routes' 
+import laporanCPLRoutes from '@/laporanCPL/routes' 
+import mataKuliahRoutes from '@/mataKuliah/routes' 
 
 const GlobalRoutes = () => {
   const router = useRoutes([
-    ...commonRoutes,
-    ...staticPageRoutes,
-    ...userRoutes,
-    ...roleRoutes,
-    ...homeRoutes,
-    ...mataKuliahRoutes,
-    ...programStudiRoutes,
-    ...kurikulumRoutes,
-    ...cPLRoutes,
-    ...laporanCPLRoutes,
-    ...laporanCPMKRoutes,
-    ...cPMKRoutes,
-    ...subCPMKRoutes,
-    ...semesterRoutes,
-    ...kelasRoutes,
-  ]);
-  return router;
-};
+	...commonRoutes,
+	...staticPageRoutes,
+	...userRoutes,
+	...roleRoutes,
+	...homeRoutes, 
+	...kurikulumRoutes, 
+	...programStudiRoutes, 
+	...kelasRoutes, 
+	...semesterRoutes, 
+	...riwayatAkademisRoutes, 
+	...cPMKRoutes, 
+	...subCPMKRoutes, 
+	...laporanCPMKRoutes, 
+	...cPLRoutes, 
+	...laporanCPLRoutes, 
+	...mataKuliahRoutes, 
+  ])
+  return router
+}
 
 const MobileRoutes = () => {
-  const router = useRoutes([...commonMobileRoutes]);
-  return router;
-};
+	const router = useRoutes([ 
+	  ...commonMobileRoutes, 
+  ])
+  return router
+}
 
-export { GlobalRoutes, MobileRoutes };
+export {GlobalRoutes, MobileRoutes}
