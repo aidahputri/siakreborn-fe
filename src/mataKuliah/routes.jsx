90cae2e675b072a6eaa03a5018de3bbfaad101b1
Aidah Novallia Putri <aidahptr15@gmail.com>
2024-11-05 13:37:05 +0700

    chore: prettier

--- src/mataKuliah/routes.jsx
+++ src/mataKuliah/routes.jsx
@@ -3,34 +3,46 @@
 	https://amanah.cs.ui.ac.id/research/ifml-regen
 	version 3.5.5
 */
-import RequireAuth from '@/commons/auth/RequireAuth'
+import RequireAuth from "@/commons/auth/RequireAuth";
 
-import TambahMataKuliahPage from './containers/TambahMataKuliahPage'
-import DaftarMataKuliahPage from './containers/DaftarMataKuliahPage'
-import DetailMataKuliahPage from './containers/DetailMataKuliahPage'
-import UbahMataKuliahPage from './containers/UbahMataKuliahPage'
+import TambahMataKuliahPage from "./containers/TambahMataKuliahPage";
+import DaftarMataKuliahPage from "./containers/DaftarMataKuliahPage";
+import DetailMataKuliahPage from "./containers/DetailMataKuliahPage";
+import UbahMataKuliahPage from "./containers/UbahMataKuliahPage";
 
 const mataKuliahRoutes = [
-{ 
-	path: "/matakuliah/tambah",
-	element: <RequireAuth permissionNeeded="CreateMataKuliah" ><TambahMataKuliahPage/></RequireAuth>
-}	
-,
-{ 
-	path: "/matakuliah/ubah",
-	element: <RequireAuth permissionNeeded="UpdateMataKuliah" ><UbahMataKuliahPage/></RequireAuth>
-}	
-,
-{ 
-	path: "/matakuliah",
-	element: <RequireAuth permissionNeeded="ReadMataKuliah" ><DaftarMataKuliahPage/></RequireAuth>
-}	
-,
-{ 
-	path: "/matakuliah/:id",
-	element: <RequireAuth permissionNeeded="ReadMataKuliah" ><DetailMataKuliahPage/></RequireAuth>
-}	
+  {
+    path: "/matakuliah/tambah",
+    element: (
+      <RequireAuth permissionNeeded="CreateMataKuliah">
+        <TambahMataKuliahPage />
+      </RequireAuth>
+    ),
+  },
+  {
+    path: "/matakuliah/ubah",
+    element: (
+      <RequireAuth permissionNeeded="UpdateMataKuliah">
+        <UbahMataKuliahPage />
+      </RequireAuth>
+    ),
+  },
+  {
+    path: "/matakuliah",
+    element: (
+      <RequireAuth permissionNeeded="ReadMataKuliah">
+        <DaftarMataKuliahPage />
+      </RequireAuth>
+    ),
+  },
+  {
+    path: "/matakuliah/:id",
+    element: (
+      <RequireAuth permissionNeeded="ReadMataKuliah">
+        <DetailMataKuliahPage />
+      </RequireAuth>
+    ),
+  },
+];
 
-]
-
-export default mataKuliahRoutes
+export default mataKuliahRoutes;
